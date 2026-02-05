56c49fbe1901558cf5a94d070315b71dec14c2d2
FavorskTim <favorskyi.t@ssmg.cz>
2026-02-05 09:30:29 +0100

    Uvod

--- HadaniCisla.py
+++ HadaniCisla.py
@@ -1,2 +1,13 @@
 import random
+cislo=random.randint(1,100)
+print(cislo)
+while True
+    random cislo=int(input("Zadej cislo: "))
+    if random==cislo:
+        break
+    elif random<cislo:
+        print(random je vetsi)
+    else:
+        print(random je mensi)
+print("uhodnuto na", pocet,"pokusu")
 
---
+++ Nasobilka.py
@@ -0,0 +1,16 @@
+priklady=int(input("Zadej pocet prikladu?"))
+for i in range ("pocet"):
+    nah1=random.randint(2,10)
+    nah2=random.randint(2,10)
+nas=0
+nas=nah1*nah2
+odpoved=int(input("Pocet"))
+odpoved=int(input("kolik se rovna"))
+if odpoved==nah1*nah2:
+    print("Dobre")
+    dobre+=1
+else:
+    print("Spatne")
+    spatne+=1
+
+
